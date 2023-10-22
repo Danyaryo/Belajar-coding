f5a7b0d74b184db7327a65c4505402d98e49454b
aryodany <danyaryo20@gmail.com>
2023-10-22 12:30:37 +0700

    add file function.js

---
+++ part 7-5/function.js
@@ -0,0 +1,32 @@
+// Tugas 1
+
+function shoutOut() {
+    return "Halo Function !!!"
+}
+
+console.log(shoutOut())
+
+// Tugas 2
+
+let num1 = 1
+let num2 = 2
+
+function calculateMultiply(number1, number2 ) {
+    return `Hasilnya adalah : ${number1 * number2}`
+}
+
+console.log(calculateMultiply(num1,num2))
+
+// Tugas 3
+
+let name = "agus";
+let age = 30;
+let address = "Jln. Malioboro, yogyakarta";
+let hobby = "gaming"
+
+function processSentence(name, age, address, hobby) {
+    return `nama saya ${name}, umur saya ${age}, alamat saya di ${address}, dan saya punya hobi yaitu ${hobby}`
+}
+
+
+console.log(processSentence(name, age, address, hobby))
