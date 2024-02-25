c856675781314ce5b1ce51d55f24f183c5866c2b
aryodany <danyaryo20@gmail.com>
2024-02-25 19:16:25 +0700

    Revisi Soal1

--- part-9-6/soal1.js
+++ part-9-6/soal1.js
@@ -1,22 +1,29 @@
 // Soal 1
 
 function changeMe(arr) {
-    const ChristEvans = {
-        firstName: arr[0][0],
-        lastName: arr[0][1],
-        gender: arr[0][2],
-        age: 2023 - arr[0][3]
-    } 
-    
-    const RoberDowney = {
-        firstName: arr[1][0],
-        lastName: arr[1][1],
-        gender: arr[1][2],
-        age: 'Invalid Birth Year'
+
+    for (let i = 0; i < arr.length; i++) {
+        const obj = {
+            firstName: "",
+            lastName: "",
+            gender: "",
+            age: 0,
+        }
+        for (j = 0; j < arr.length; j++) {
+            if (arr[i] === arr[j]) {
+                obj.firstName = arr[j][0]
+                obj.lastName = arr[j][1]
+                obj.gender = arr[j][2]
+            }
+            if (obj.firstName == "Christ") {
+                obj.age = 2023 - 1982
+            } else {
+                obj.age = "Invalid Birth Year"
+            }
+        }
+        console.log({...obj})
     }
-    console.log("1. Christ Evans: ",
-    "Christ Evans :", ChristEvans)
-    console.log("2. Robert Downey:", "Robert Downey :", RoberDowney)
+
 }
 
 changeMe([['Christ', 'Evans', 'Male', '1982'], ['Robert', 'Downey', 'Male',]])
